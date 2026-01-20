4c55459a16b2da906419b0ff58cf512fe72c40f8
ekwoo <95988943+ekwoo@users.noreply.github.com>
2026-01-21 01:20:49 +0900

    윈도우 11 새 IME 대응

--- ECLIPSE_PLUGIN/ImeCaret/src/imecaret/util/IMEUtils.java
+++ ECLIPSE_PLUGIN/ImeCaret/src/imecaret/util/IMEUtils.java
@@ -13,8 +13,15 @@ public class IMEUtils {
 	/**
 	 * This message is sent by an application to an IME window to determine whether the IME is open.
 	 * lParam Not Used
+	 * 이전 버전의 Microsoft IME설정을 켰다면 아래값을 사용한다.
 	 */
 	public static final int IMC_GETOPENSTATUS = 5;
+	/**
+	 * This message is sent by an application to an IME window to obtain the current conversion mode. The window retrieves the current conversion mode from the current input context.
+	 * lParam Not Used
+	 * 윈도우 11의 새 IME를 사용하면 아래값을 사용한다.
+	 */
+	public static final int IMC_GETCONVERSIONMODE = 1;
 	
 	/**
 	 * check current window is IME Mode
@@ -26,7 +33,7 @@ public class IMEUtils {
 		//IME 윈도우 핸들 얻기
 		long hime = OS.ImmGetDefaultIMEWnd(hwnd);
 		//IME가 열려있는지 확인
-		long status = OS.SendMessage(hime, WM_IME_CONTROL, IMC_GETOPENSTATUS, 0);
+		long status = OS.SendMessage(hime, WM_IME_CONTROL, IMC_GETCONVERSIONMODE, 0);
 		//0이 아니면 IME OPEN
 		return status != 0;
 	}
